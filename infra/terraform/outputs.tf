f26d31b56c99426071c48d1da0b37a18ac608bcd
stackgen-automation <automation@stackgen.com>
2024-09-28 09:58:05 +0000

    Automated commit

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,10 +1,4 @@
 
-output "aws_s3_kitty_blr_stackgen_1_kms_arn" {
-  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.kms_arn
-  sensitive = false
-}
-
-
 output "aws_s3_kitty_blr_stackgen_1_arn" {
   value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.arn
   sensitive = false
@@ -23,26 +17,8 @@ output "aws_s3_kitty_blr_stackgen_1_bucket_website_endpoint" {
 }
 
 
-output "aws_lambda_hello_kitty_function_blr_stackgen_1_function_arn" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_function_blr_stackgen_1_function_name" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_name
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_function_blr_stackgen_1_function_url" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_url
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_function_blr_stackgen_1_invoke_arn" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
+output "aws_s3_kitty_blr_stackgen_1_kms_arn" {
+  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.kms_arn
   sensitive = false
 }
 
@@ -67,5 +43,29 @@ output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_stackgen_83_arn
 
 output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_stackgen_83_name" {
   value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.name
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_function_blr_stackgen_1_function_url" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_url
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_function_blr_stackgen_1_invoke_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_function_blr_stackgen_1_function_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_function_blr_stackgen_1_function_name" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_name
   sensitive = false
 }
